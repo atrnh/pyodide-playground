07f75bcc96be0e9e3072e7d0685f4f63a27ae7b5
Ashley Trinh <atrnh@users.noreply.github.com>
2025-01-28 16:24:18 -0800

    update example

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
 import PythonPlayground from "./components/PythonPlayground";
 
+const initialCode = `\
+a = 1
+b = 2
+print(a + b)
+# Add more code here!\
+`;
+
 function App() {
   return (
     <>
-      <PythonPlayground initialCode="print('Hello, world!')" />
-      <h2>Another one</h2>
-      <PythonPlayground initialCode="n = sum([1, 2, 3])" />
+      <h1>Python Playground Demo</h1>
+      <p>
+        Press the
+        {" "}
+        <b>Run</b>
+        {" "}
+        button to execute the Python code below.
+      </p>
+      <p> You can also add more code using the editor!</p>
+      <p>
+        Press
+        {" "}
+        <b>Reset</b>
+        {" "}
+        to reset the code to its initial state, and
+        {" "}
+        <b>Copy</b>
+        {" "}
+        to copy the code to the clipboard.
+      </p>
+      <PythonPlayground
+        initialCode={initialCode}
+      />
     </>
   );
 }
