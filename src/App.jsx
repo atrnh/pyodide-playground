62c00683c7f61422c9b583ecb2a1213a0c77bfe1
Ashley Trinh <atrnh@users.noreply.github.com>
2025-01-28 16:18:53 -0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import PythonPlayground from "./components/PythonPlayground";
+import PythonPlayground, { PythonPlayground2 } from "./components/PythonPlayground";
+import PyodideProvider from "./py/PyodideProvider";
 
 function App() {
   return (
     <>
+      <h2>Version 2</h2>
+      <PyodideProvider pyodideUrl="https://cdn.jsdelivr.net/pyodide/v0.27.2/full/pyodide.mjs">
+        <PythonPlayground2
+          initialCode="print('Hello, world!')"
+        />
+      </PyodideProvider>
+      <h2>Version 1</h2>
       <PythonPlayground initialCode="print('Hello, world!')" />
-      <h2>Another one</h2>
-      <PythonPlayground initialCode="n = sum([1, 2, 3])" />
     </>
   );
 }
